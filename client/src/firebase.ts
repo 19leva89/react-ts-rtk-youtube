8762ceaba73e387ddb9152f7a04b4105a96d4f96
Dmitry Soboliev <19leva89@gmail.com>
2024-09-12 12:45:08 +0300

    add env.FIREBASE

--- client/src/firebase.ts
+++ client/src/firebase.ts
@@ -2,13 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-	// apiKey: process.env.FIREBASE_KEY,
-	apiKey: "AIzaSyAe8kRS0Jhoc2A9h_h845xhrZd6ngAX10k",
-	authDomain: "dima-tube.firebaseapp.com",
-	projectId: "dima-tube",
-	storageBucket: "dima-tube.appspot.com",
-	messagingSenderId: "831386683441",
-	appId: "1:831386683441:web:4236d246f73ac6b0ce2d10",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
